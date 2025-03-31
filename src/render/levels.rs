583bfe0b36ec803a184773fbb5aa084a3d8d46c5
[Error_27] <error27dev@gmail.com>
2025-03-30 21:27:00 -0400

    clean up code by removing unneccessary function split

--- src/render/levels.rs
+++ src/render/levels.rs
@@ -13,46 +13,37 @@ pub fn draw_level(
     source: Option<Rect>,
 ) {
     let lvl = &res.levels[level_idx];
+    let tilesets = &res.tilesets;
 
     for layer in &lvl.layers {
-        draw_layer(layer, &res.tilesets, &textures, position, source);
-    }
-}
+        let tileset = tilesets.get(&layer.tileset_id).unwrap();
+        let tex = &textures[tileset.texture_index as usize].0;
 
-fn draw_layer(
-    layer: &LdtkLayerInstance,
-    tilesets: &HashMap<String, LdtkTileset>,
-    textures: &[(Texture2D, &str)],
-    position: Vec2,
-    source: Option<Rect>,
-) {
-    let tileset = tilesets.get(&layer.tileset_id).unwrap();
-    let tex = &textures[tileset.texture_index as usize].0;
+        for t in &layer.grid_tiles {
+            if let Some(s) = source {
+                let grid_x = t.px_coords[0] as f32 / tileset.tile_grid_size as f32;
+                let grid_y = t.px_coords[1] as f32 / tileset.tile_grid_size as f32;
 
-    for t in &layer.grid_tiles {
-        if let Some(s) = source {
-            let grid_x = t.px_coords[0] as f32 / tileset.tile_grid_size as f32;
-            let grid_y = t.px_coords[1] as f32 / tileset.tile_grid_size as f32;
-
-            // Don't render outside of the specified source rectangle
-            if grid_x < s.x || grid_x >= s.x + s.w || grid_y < s.y || grid_y >= s.y + s.h {
-                continue;
+                // Don't render outside of the specified source rectangle
+                if grid_x < s.x || grid_x >= s.x + s.w || grid_y < s.y || grid_y >= s.y + s.h {
+                    continue;
+                }
             }
+            draw_texture_ex(
+                tex,
+                t.px_coords[0] as f32 + position.x,
+                t.px_coords[1] as f32 + position.y,
+                WHITE,
+                DrawTextureParams {
+                    source: Some(Rect {
+                        x: t.src_coords[0] as f32,
+                        y: t.src_coords[1] as f32,
+                        w: tileset.tile_grid_size as f32,
+                        h: tileset.tile_grid_size as f32,
+                    }),
+                    ..Default::default()
+                },
+            );
         }
-        draw_texture_ex(
-            tex,
-            t.px_coords[0] as f32 + position.x,
-            t.px_coords[1] as f32 + position.y,
-            WHITE,
-            DrawTextureParams {
-                source: Some(Rect {
-                    x: t.src_coords[0] as f32,
-                    y: t.src_coords[1] as f32,
-                    w: tileset.tile_grid_size as f32,
-                    h: tileset.tile_grid_size as f32,
-                }),
-                ..Default::default()
-            },
-        );
     }
 }
