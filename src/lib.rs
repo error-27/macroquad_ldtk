cdc126f7ec8372c1843f01e2fbf611e97cb2b0b3
[Error_27] <error27dev@gmail.com>
2025-04-03 22:00:38 -0400

    remove unnecessary module

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 pub mod error;
+pub mod levels;
 pub mod load;
-pub mod render;
 pub mod types;
 
 mod parser;
 
 pub mod prelude {
+    pub use crate::error::Error;
+    pub use crate::levels::*;
     pub use crate::load::*;
-    pub use crate::render::levels::*;
 }
