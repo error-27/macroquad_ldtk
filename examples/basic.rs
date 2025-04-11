988ed4713f26df73df2147e7615bef8756c1e82b
[Error_27] <error27dev@gmail.com>
2025-04-10 21:55:06 -0400

    update basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,30 +1,48 @@
-use macroquad::prelude::*;
+use macroquad::{prelude::*, window::Conf};
 use macroquad_ldtk::prelude::*;
 
-#[macroquad::main("Basic LDtk Example")]
+const VIRTUAL_WIDTH: i32 = 270; // These are the dimensions of a single level.
+const VIRTUAL_HEIGHT: i32 = 252; // The level is rendered onto a canvas of this size, then upscaled.
+const SCALE: i32 = 3;
+
+#[macroquad::main(window_conf)]
 async fn main() {
     let main_tileset = load_texture("assets/kenney_platformer.png").await.unwrap();
-    main_tileset.set_filter(FilterMode::Linear);
+    main_tileset.set_filter(FilterMode::Nearest);
 
-    let tilesets = [(main_tileset, "kenney_platformer.png")];
+    let tilesets = [(main_tileset, "kenney_platformer.png")]; // This array of tuples is passed to the loading and drawing functions.
 
-    let res = load_project("assets/platformer_example.ldtk", &tilesets).unwrap();
+    let res = load_project("assets/basic.ldtk", &tilesets).unwrap();
 
-    let current_level = 0;
+    let level = (0, 0); // The level is stored by its coordinates (except in Horizontal and Vertical layouts; see docs)
 
     // Load all coins into the level
     let coins: Vec<Coin> = res
-        .get_entities((current_level, 0))
+        .get_entities(level)
         .iter()
         .map(|me| Coin {
             position: me.px_coords,
         })
         .collect();
 
-    loop {
-        clear_background(BLACK);
-        res.draw_level((current_level, 0), &tilesets, Vec2::new(0.0, 0.0), None);
+    // Create render targets to upscale the level
+    let render_target = render_target(VIRTUAL_WIDTH as u32, VIRTUAL_HEIGHT as u32);
+    render_target.texture.set_filter(FilterMode::Nearest);
 
+    let mut render_target_cam = Camera2D::from_display_rect(Rect::new(
+        0.0,
+        0.0,
+        VIRTUAL_WIDTH as f32,
+        VIRTUAL_HEIGHT as f32,
+    ));
+    render_target_cam.render_target = Some(render_target.clone());
+
+    loop {
+        set_camera(&render_target_cam);
+        clear_background(BLUE);
+        res.draw_level(level, &tilesets, Vec2::new(0.0, 0.0), None);
+
+        // Draw coins
         for c in &coins {
             draw_texture_ex(
                 &tilesets[0].0,
@@ -43,7 +61,33 @@ async fn main() {
             );
         }
 
+        set_default_camera();
+        draw_texture_ex(
+            &render_target.texture,
+            (screen_width() - (VIRTUAL_WIDTH * SCALE) as f32) * 0.5,
+            (screen_height() - (VIRTUAL_HEIGHT * SCALE) as f32) * 0.5,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(vec2(
+                    (VIRTUAL_WIDTH * SCALE) as f32,
+                    (VIRTUAL_HEIGHT * SCALE) as f32,
+                )),
+                flip_y: true,
+                ..Default::default()
+            },
+        );
+
         next_frame().await
+    }
+}
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Basic LDtk Example".to_string(),
+        window_width: VIRTUAL_WIDTH * SCALE,
+        window_height: VIRTUAL_HEIGHT * SCALE,
+        window_resizable: false,
+        ..Default::default()
     }
 }
 
